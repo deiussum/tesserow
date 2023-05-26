54da6b1be2bcee66ecbf3bd99b729c07c6c5b2f9
Daniel Jenkins <deiussum@gmail.com>
2023-05-26 09:14:16 -0500

    Added initial WPF project

---
+++ Deiussum.PatternMaker.WPF/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Deiussum.PatternMaker.WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
---
+++ Deiussum.PatternMaker.WPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Deiussum.PatternMaker.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void ExitApp(object sender, RoutedEventArgs ea) {
+        Application.Current.Shutdown();
+    }
+}
